75ba08150abafd4839458d67f80cc87bdd330ad1
Josh Kohl <joshdkohl@gmail.com>
2022-01-06 17:52:33 -0500

    Cleaned up demo page

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-        <Header title="I bought a domain." />
+        <Header title="Hi. I bought a domain." />
       </main>
 
     </div>
