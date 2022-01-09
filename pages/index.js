db55fdfc1d5c16a861a90c7bc6d46f122b2978fc
Josh Kohl <joshdkohl@gmail.com>
2022-01-09 18:59:27 -0500

    Troubleshooting deployment: Changed component import in index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,7 @@
 import Head from 'next/head';
 import Header from '@components/Header';
-import RootWindow from '@components/RootWindow';
+// import RootWindow from '@components/RootWindow';
+import RootWindow from '../components/RootWindow';
 
 export default function Home() {
   return (
