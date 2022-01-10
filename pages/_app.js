8b16c22086e3ebcbd3b21a7a860625216bb1417c
Josh Kohl <joshdkohl@gmail.com>
2022-01-09 19:15:28 -0500

    CSS Troubleshooting. Removed header.css file and imports

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,5 @@
 import React from 'react';
 import '@styles/globals.css';
-import '@styles/header.css';
-
 
 const Application = ({ Component, pageProps }) => {
   return <Component {...pageProps} />;
