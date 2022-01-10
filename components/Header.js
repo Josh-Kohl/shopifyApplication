ffb875d51f81dee3fecbb29db8d60c6296b50d4b
Josh Kohl <joshdkohl@gmail.com>
2022-01-09 19:37:33 -0500

    CSS Styling

--- components/Header.js
+++ components/Header.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import styles from './Header.module.css';
 
-
 const Header = () => {
 
   //Header Bar to select Rover, Browse page, Liked Page
 
   return (
-    <div className='styles.header'>
+    <div className={styles.header}>
       <h1>Header Component</h1>
     </div>
   );
