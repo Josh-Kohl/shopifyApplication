f2979d5bd89f00cddada73cd474bb00c7978b0d7
Josh Kohl <joshdkohl@gmail.com>
2022-01-09 21:43:02 -0500

    Syntax correction for credentials import

--- components/rootWindow.js
+++ components/rootWindow.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import credentials from '../ credentials';
+import credentials from '../credentials';
 
 import Header from './Header';
 import BrowseWindow from './BrowseWindow';
